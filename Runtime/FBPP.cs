dbc482e8e651522c8bde68b429d16342b72bc0f0
pheeyam <sameerahmed1472@gmail.com>
2024-10-20 13:55:13 +0500

    added functions for unity specific data types like vec3

--- Runtime/FBPP.cs
+++ Runtime/FBPP.cs
@@ -21,7 +21,7 @@ public static class FBPP
 
     private static FBPPFileModel _latestData;
     private static StringBuilder _sb = new StringBuilder();
-    
+
     const string String_Empty = "";
 
     #region Init
@@ -81,6 +81,77 @@ public static class FBPP
         AddDataToSaveFile(key, value);
     }
 
+
+    public static void SetKeyCode(string key, KeyCode value)
+    {
+        FBPP.SetInt(key, (int)value);
+    }
+    public static KeyCode GetKeyCode(string key)
+    {
+        return (KeyCode)FBPP.GetInt(key);
+    }
+    public static void DeleteKeyCode(string key)
+    {
+        FBPP.DeleteInt(key);
+    }
+    public static bool HasKeyForKeyCode(string key)
+    {
+        return FBPP.HasKeyForInt(key);
+    }
+    // Vector3
+    public static void SetVector3(string key, Vector3 value)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            FBPP.SetFloat(key + "_" + i, value[i]);
+        }
+    }
+    public static Vector3 GetVector3(string key)
+    {
+        Vector3 vec = new();
+        for (int i = 0; i < 3; i++)
+        {
+            vec[i] = FBPP.GetFloat(key + "_" + i);
+        }
+        return vec;
+    }
+    public static bool HasKeyForVector3(string key)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            bool has = FBPP.HasKeyForFloat(key + "_" + i);
+            if (!has) return false;
+        }
+        return true;
+    }
+    // Quaternion
+    public static void SetQuaternion(string key, Quaternion value)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            FBPP.SetFloat(key + "_" + i, value[i]);
+        }
+    }
+    public static Quaternion GetQuaternion(string key)
+    {
+        Quaternion vec = new();
+        for (int i = 0; i < 4; i++)
+        {
+            vec[i] = FBPP.GetFloat(key + "_" + i);
+        }
+        return vec;
+    }
+    public static bool HasKeyForQuaternion(string key)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            bool has = FBPP.HasKeyForFloat(key + "_" + i);
+            if (!has) return false;
+        }
+        return true;
+    }
+
+
     public static bool GetBool(string key, bool defaultValue = default(bool))
     {
         return (bool)GetDataFromSaveFile(key, defaultValue);
@@ -110,6 +181,8 @@ public static class FBPP
     {
         return GetSaveFile().HasKeyFromObject(key, default(bool));
     }
+
+
 
     public static void DeleteKey(string key)
     {
@@ -198,7 +271,7 @@ public static class FBPP
         return Path.Combine(_config.GetSaveFilePath(), _config.SaveFileName);
     }
 
-  
+
 
     public static string GetSaveFileAsJson()
     {
@@ -239,7 +312,7 @@ public static class FBPP
     }
     private static void WriteToSaveFile(string data)
     {
-        var tw = new StreamWriter( GetSaveFilePath());
+        var tw = new StreamWriter(GetSaveFilePath());
         if (_config.ScrambleSaveData)
         {
             data = DataScrambler(data);
